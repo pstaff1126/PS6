2cabe58dc4538cbbf2d18832e369acf1a5049551
pstaff1126 <pstaff@udel.edu>
2015-11-13 14:43:36 -0500

    PS6

--- PS6_DAL/src/main/java/base/PersonDAL.java
+++ PS6_DAL/src/main/java/base/PersonDAL.java
@@ -11,32 +11,132 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import domain.PersonDomainModel;
+import domain.StudentDomainModel;
 import util.HibernateUtil;
 
 public class PersonDAL {
 
 	public static PersonDomainModel addPerson(PersonDomainModel per) {
-		//PS6 - please implement
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trans = null;
+		int eID = 0;
+		try {
+			trans = session.beginTransaction();
+			session.save(per);
+			trans.commit();
+		} catch (HibernateException e) {
+			if (trans != null) {
+				trans.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return per;
 	}
+	
 
 	public static ArrayList<PersonDomainModel> getPersons() {
-		//PS6 - please implement		
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trans = null;
+		StudentDomainModel studentmodel = null;
+		ArrayList<PersonDomainModel> person = new ArrayList<PersonDomainModel>();
+
+		try {
+			trans = session.beginTransaction();
+
+			List students = session.createQuery("FROM PersonDomainModel").list();
+			for (Iterator iterator = students.iterator(); iterator.hasNext();) {
+				PersonDomainModel per = (PersonDomainModel) iterator.next();
+				person.add(per);
+
+			}
+
+			trans.commit();
+		} catch (HibernateException e) {
+			if (trans != null)
+				trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return person;
 
 	}
 
 	public static PersonDomainModel getPerson(UUID perID) {
-		//PS6 - please implement		
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trans = null;
+		PersonDomainModel person = null;
+
+		try {
+			trans = session.beginTransaction();
+
+			Query query = session.createQuery("from PersonDomainModel where PersonID = :id ");
+			query.setParameter("id", perID);
+
+			person = (PersonDomainModel) query.list().get(0);
+
+			trans.commit();
+
+		} catch (IndexOutOfBoundsException ex) {
+			person = null;
+		} catch (HibernateException e) {
+			if (trans != null)
+				trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return person;
 	}
 
 	public static void deletePerson(UUID perID) {
-		//PS6 - please implement
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trans = null;
+		PersonDomainModel person = null;
+
+		try {
+			trans = session.beginTransaction();
+
+			PersonDomainModel per = (PersonDomainModel) session.get(PersonDomainModel.class, perID);
+			if (per==null)
+			{
+				// Nothing to delete, not found
+				return;
+			}
+			
+			session.delete(per);
+
+			trans.commit();
+		} catch (HibernateException e) {
+			if (trans != null)
+				trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 	}
 
 	public static PersonDomainModel updatePerson(PersonDomainModel per) {
-		//PS6 - please implement		
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction trans = null;
+		PersonDomainModel person = null;
+
+		try {
+			trans = session.beginTransaction();
+
+			session.update(per);
+
+			trans.commit();
+		} catch (HibernateException e) {
+			if (trans != null)
+				trans.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return per;
 	}
 }
--- PS6_DAL/src/test/java/base/Student_Test.java
+++ PS6_DAL/src/test/java/base/Student_Test.java
@@ -2,16 +2,35 @@ package base;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import domain.PersonDomainModel;
+import domain.StudentDomainModel;
+
 public class Student_Test {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		Date dDate = null;
+		try {
+			dDate = new SimpleDateFormat("yyyy-MM-dd").parse("1993-11-26");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		StudentDomainModel per1 = new StudentDomainModel();
+		per1.setFirstName("Pat");
+		per1.setLastName("Stafford");
+		per1.setDOB(dDate);
+		
 	}
 
 	@AfterClass
@@ -27,8 +46,62 @@ public class Student_Test {
 	}
 
 	@Test
-	public void test() {
-		assertTrue(1==1);
+	public void AddStudentTest()
+	{	
+		Date dDate = null;
+		try {
+			dDate = new SimpleDateFormat("yyyy-MM-dd").parse("1972-07-31");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		StudentDomainModel per1 = new StudentDomainModel();
+		per1.setFirstName("Pat");
+		per1.setLastName("Stafford");
+		per1.setDOB(dDate);
+		
+		StudentDomainModel per;		
+		per = StudentDAL.getStudent(per1.getStudentID());		
+		assertNull("The Student shouldn't have been in the database",per);		
+		StudentDAL.addStudent(per1);	
+		
+		per = StudentDAL.getStudent(per1.getStudentID());
+		System.out.println(per1.getStudentID() + " found");
+		assertNotNull("The Student should have been added to the database",per);
+	}
+	@Test
+	public void UpdateStudentTest()
+	{		
+		StudentDomainModel per;
+		final String C_LASTNAME = "Smith";
+		
+		per = StudentDAL.getStudent(per1.getStudentID());		
+		assertNull("The Student shouldn't have been in the database",per);		
+		StudentDAL.addStudent(per1);	
+		
+		per1.setLastName(C_LASTNAME);
+		StudentDAL.updateStudent(per1);
+		
+		per = StudentDAL.getStudent(per1.getStudentID());
+
+		assertTrue("Name Didn't Change",per1.getLastName() == C_LASTNAME);
+	}
+	@Test
+	public void DeleteStudentTest()
+	{		
+		StudentDomainModel per;		
+		per = StudentDAL.getStudent(per1.getStudentID());		
+		assertNull("The Student shouldn't have been in the database",per);	
+		
+		StudentDAL.addStudent(per1);			
+		per = StudentDAL.getStudent(per1.getStudentID());
+		System.out.println(per1.getStudentID() + " found");
+		assertNotNull("The Student should have been added to the database",per);
+		
+		StudentDAL.deleteStudent(per1.getStudentID());
+		per = StudentDAL.getStudent(per1.getStudentID());		
+		assertNull("The Student shouldn't have been in the database",per);	
+		
 	}
 
 }
